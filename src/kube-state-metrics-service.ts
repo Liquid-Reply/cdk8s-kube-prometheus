fd5974bd0da0a1174167e1c4092e1a7df4cb4b5a
qwerformat <fynn.chamier@gmail.com>
2022-02-14 11:03:46 +0100

    kube-state-metrics-service

--- src/kube-state-metrics-service.ts
+++ src/kube-state-metrics-service.ts
@@ -8,7 +8,7 @@ export class kubeStateMetricsservice extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    // define resources here
+    //Port namen sind Int nicht wie String
   const frontends =  new kplus.Service(this,"kube-state-metrics", {ports: [{name: "https-main", port: 8443,/*targetPort:https-main*/}, {name: "https-self", port: 9443,/*targetPort: https-self */}],clusterIP: "None",metadata:{name: "kube-state-metrics", namespace:"monitoring", labels: {"app.kubernetes.io/componen": "exporter", "app.kubernetes.io/name":"kube-state-metrics", "app.kubernetes.io/part-of":"kube-prometheus", "app.kubernetes.io/version":"2.3.0"}}})
   frontends.addSelector("app.kubernetes.io/component", "exporter");
   frontends.addSelector("app.kubernetes.io/name", "kube-state-metrics");
