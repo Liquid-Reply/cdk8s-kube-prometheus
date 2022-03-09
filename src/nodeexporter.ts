2a1027011a16a711d5cd38688de4152b524dbce0
Daniel Aberger <d.aberger@reply.de>
2022-03-09 08:53:24 +0100

    fix port var in nodeexporter container args

--- src/nodeexporter.ts
+++ src/nodeexporter.ts
@@ -94,9 +94,9 @@ export class Nodeexporter extends cdk8s.Chart {
                 image: `quay.io/brancz/kube-rbac-proxy:v${rbacProxyVersion}`,
                 args: [
                   "--logtostderr",
-                  "--secure-listen-address=[$(IP)]:${port}",
+                  `--secure-listen-address=[$(IP)]:${port}`,
                   "--tls-cipher-suites=TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
-                  "--upstream=http://127.0.0.1:${port}/",
+                  `--upstream=http://127.0.0.1:${port}/`,
                 ],
                 ports: [
                   {
